fe78c8684c164a5a4d44687445a8346b995e56cd
Sumit Randhe <114733096+sumitrandhe@users.noreply.github.com>
2023-01-02 13:34:45 +0530

    Login Box Uploded

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 const op = document.getElementById('op');
+
+
+//************************Login Box JavaScript And jQuery*****************
+
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let flag = 1
+
+function validate() {
+    if (email.value == "") {
+        document.getElementById("emailError").innerHTML = "Username is Empty"
+        flag = 0
+    } else if (email.value.length < 4) {
+        document.getElementById("emailError").innerHTML = "Username must be more than 3 character"
+        flag = 0
+    } else {
+
+        document.getElementById("emailError").innerHTML = ""
+        flag = 1
+    }
+
+    if (password.value == "") {
+        document.getElementById("passError").innerHTML = "Password is Empty"
+        flag = 0
+    } else {
+        document.getElementById("passError").innerHTML = ""
+    }
+
+    if (flag) {
+        return true
+    } else {
+        return false
+    }
+   
+}
+
+$("#close-btn").click(function(){
+    $("#loginBox-div").hide(200)
+})
+
+//*********************Login Box JavaScript & jQuery End************************************ */
