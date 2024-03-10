197da09c414144c66040c13de258ae23bb79eeef
Rahul Kumar <rahulkumar17082000@gmail.com>
2024-03-10 22:48:54 +0530

    Setup all user component

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { UserComponent } from './user/user.component';
+import { HeroComponent } from './hero/hero.component';
+import { SharedModule } from '../shared/shared.module';
+import { SearchComponent } from './hero/search/search.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { TestinomialComponent } from './testinomial/testinomial.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UserComponent, HeroComponent, SearchComponent, HowItWorksComponent, WhyUsComponent, TestinomialComponent, FooterComponent, HeaderComponent],
+  imports: [SharedModule],
+  exports: [UserComponent],
 })
-export class UserModule { }
+export class UserModule {}
